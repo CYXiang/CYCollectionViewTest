bfc011b73cc6e027c27102d1cbcf8922319e9cab
cyx <applekaifa@go.local>
2016-03-04 16:23:34 +0800

    初始化项目
    初始化项目

---
+++ CYCollectionViewTest/CYCollectionViewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CYCollectionViewTest
+//
+//  Created by apple开发 on 16/3/4.
+//  Copyright © 2016年 cyx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
